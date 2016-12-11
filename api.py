a676f6640f19bc3f348c44e893825a4139f58da1
Matt Swan <mattbryanswan@gmail.com>
2016-12-11 04:30:10 +0000

    final commit before assignment deadline

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 import psycopg2
 import os
+import json
 
 SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://w205_final:w205_final@w205-final.cz7z0nmeyqpf.us-east-1.rds.amazonaws.com:5432/w205_final"
 e = create_engine(SQLALCHEMY_DATABASE_URI)
@@ -13,9 +14,12 @@ api = Api(app)
 class Drugs(Resource):
     def get(self):
         conn = e.connect()
-        query = conn.execute("SELECT DISTINCT(drug_name) FROM loader_brands LIMIT 1000;")
-        result = {"drugs": [i[0] for i in query.cursor.fetchall()]}
-        return result
+        query = conn.execute("(select drug_name, brand_name as name, 'brand' as type from fda_brands union select drug_name, generic_name as name, 'generic' as type from fda_generics) order by name asc;")
+        data = {}
+        for i in query.cursor.fetchall():
+            data[i[1]] = [{'drug': i[0]}, {'type': i[2]}]
+
+        return data
 
 api.add_resource(Drugs, "/drugs")
 
